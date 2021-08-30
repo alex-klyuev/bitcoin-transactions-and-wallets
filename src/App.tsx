15f6d1d734f5e3db21f8b67b81125efb21636056
Alex Klyuev <aklyuev96@gmail.com>
2021-08-30 13:48:52 -0700

    complete address list

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ const App = (): ReactElement => {
   // but we'll keep it here to start for the purposes of simulating the
   // Bitcoin transaction verification and chaining design at a high level
   const [walletTracker, setWalletTracker] = useState<WalletTracker>({});
+  const [addressList, setAddressList] = useState<string[]>([]);
 
   // handlers
   const createNewWallet = (username: string): void => {
@@ -31,26 +32,26 @@ const App = (): ReactElement => {
       privateKey
     } = generateWallet();
 
-    const newWallet = {
-      [address]: {
-        username,
-        pubKey: publicKey,
-        privKey: privateKey
-      }
-    };
-
     // add to wallet tracker
-    const updatedWalletTracker = {};
-    Object.assign(updatedWalletTracker, walletTracker, newWallet);
-    setWalletTracker(updatedWalletTracker);
+
+    walletTracker[address] = {
+      username,
+      pubKey: publicKey,
+      privKey: privateKey
+    }
+    setWalletTracker({...walletTracker});
+
+    // add address to list
+    addressList.push(address);
+    setAddressList([...addressList]);
   };
 
   return (
     <div>
       <NewWalletForm createNewWallet={createNewWallet} />
-      <AddressList wallets={walletTracker} />
+      <AddressList addressList={addressList} />
     </div>
   );
-}
+};
 
 export default App;
