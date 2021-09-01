7575f49637154d14049bf6270822e01b1b77112b
Alex Klyuev <aklyuev96@gmail.com>
2021-09-01 16:31:54 -0700

    complete genesis deposit function

--- src/classes/Genesis.ts
+++ src/classes/Genesis.ts
@@ -1,6 +1,7 @@
 import { createHash, createSign } from 'crypto';
 import generateWallet from '../functions/generateWallet';
 import TXOutput from './TXOutput';
+import Transaction from './Transaction';
 
 // Genesis is going to have similar functionality to a normal wallet,
 // except it will lack a UI and have some extra functions.
@@ -48,19 +49,45 @@ class Genesis {
     const hashFunction1 = createHash('sha256');
     hashFunction1.update(inputTXID);
     hashFunction1.update(address);
-    const midHash = hashFunction1.digest('hex');
+    const midHash1 = hashFunction1.digest('hex');
 
     // sign the hash
-    const sign = createSign('sha256');
-    sign.update(midHash);
-    sign.end();
-    const sig = sign.sign(this.privateKey, 'hex');
+    const signFunction1 = createSign('sha256');
+    signFunction1.update(midHash1);
+    signFunction1.end();
+    const sig1 = signFunction1.sign(this.privateKey, 'hex');
 
     // hash signature to convert into TXID format
     const hashFunction2 = createHash('sha256');
-    const outputTXID = hashFunction2.update(sig).digest('hex');
+    const outputUserTXID = hashFunction2.update(sig1).digest('hex');
+
+    // now repeat the process with the Genesis address for the second UTXO
+    const hashFunction3 = createHash('sha256');
+    hashFunction3.update(inputTXID);
+    hashFunction3.update(this.address);
+    const midHash2 = hashFunction3.digest('hex');
+    const signFunction2 = createSign('sha256');
+    signFunction2.update(midHash2);
+    signFunction2.end();
+    const sig2 = signFunction2.sign(this.privateKey, 'hex');
+    const hashFunction4 = createHash('sha256');
+    const outputGenesisTXID = hashFunction4.update(sig2).digest('hex');
+
+    // create the two UTXOs
+    const userUTXO = new TXOutput(outputUserTXID, address, value);
+    const newGenesisUTXO = new TXOutput(
+      outputGenesisTXID,
+      this.address,
+      this.balance() - value
+    );
+
+    // create the transaction
+    const transaction = new Transaction();
+    transaction.inputs.push(this.UTXO);
+    transaction.outputs.push(userUTXO, newGenesisUTXO);
 
     // update UTXO pointer
+    this.UTXO = newGenesisUTXO;
   }
 }
 
